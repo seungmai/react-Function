8df648637a56cd7dd1985b1100f5011d44d366a5
seungmai <76721552+seungmai@users.noreply.github.com>
2022-02-21 12:59:09 +0900

    infinity-scroll 무한스크롤 구현

---
+++ infinity-scroll_prac/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Infinity from "./components/Infinity";
+
+function App() {
+  return (
+    <div className="App">
+      <Infinity />
+    </div>
+  );
+}
+
+export default App;
