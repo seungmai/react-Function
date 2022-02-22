65c3a434f1b3a123471c36cf9fd06b023b65a89d
seungmai <76721552+seungmai@users.noreply.github.com>
2022-02-22 13:00:15 +0900

    ToDo 리스트 구현(삭제, 체크  및 추가)

---
+++ todo-list/src/components/ToDoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+import ToDoItem from "./ToDoItem";
+const ToDoList = () => {
+  return (
+    <ToDoListBlock>
+      <ToDoItem text="프로젝트 생성하기" done={true} />
+      <ToDoItem text="컴포넌트 스타일링 하기" done={true} />
+      <ToDoItem text="Context 만들기" done={true} />
+      <ToDoItem text="기능 구현하기" done={true} />
+    </ToDoListBlock>
+  );
+};
+
+const ToDoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+export default ToDoList;
