f8ea82122457887e35f7f9cb72e30b356e7088d8
seungmai <76721552+seungmai@users.noreply.github.com>
2022-02-23 12:11:36 +0900

    ToDo 리스트

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { createGlobalStyle } from "styled-components";
+import React from 'react'
+import {createGlobalStyle} from 'styled-components'
 
-import ToDoTemplate from "./components/ToDoTemplate";
-import ToDoHead from "./components/ToDoHead";
-import ToDoList from "./components/ToDoList";
-import ToDoCreacte from "./components/ToDoCreate";
+import ToDoTemplate from './components/ToDoTemplate'
+import ToDoHead from './components/ToDoHead'
+import ToDoList from './components/ToDoList'
+import ToDoCreacte from './components/ToDoCreate'
 
 function App() {
-  return (
-    <div className="App">
-      <>
-        <GlobalStyle />
-        <ToDoTemplate>
-          <ToDoHead />
-          <ToDoList />
-          <ToDoCreacte />
-        </ToDoTemplate>
-      </>
-    </div>
-  );
+	return (
+		<div className="App">
+			<>
+				<GlobalStyle />
+				<ToDoTemplate>
+					<ToDoHead />
+					<ToDoList />
+					<ToDoCreacte />
+				</ToDoTemplate>
+			</>
+		</div>
+	)
 }
 
 const GlobalStyle = createGlobalStyle`
   body {
     background: #e9ecef;
   }
-`;
+`
 
-export default App;
+export default App
--- todo-list/src/components/ToDoTemplate.js
+++ todo-list/src/components/ToDoTemplate.js
@@ -1,22 +1,22 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
-const ToDoTemplate = ({ children }) => {
-  return <ToDoTemplateBlock>{children}</ToDoTemplateBlock>;
-};
+const ToDoTemplate = ({children}) => {
+	return <ToDoTemplateBlock>{children}</ToDoTemplateBlock>
+}
 
 const ToDoTemplateBlock = styled.div`
-  width: 512px;
-  height: 768px;
-  position: relative;
-  background: white;
-  border-radius: 16px;
-  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
-  margin: 0 auto;
-  margin-top: 96px;
-  margin-bottom: 32px;
-  display: flex;
-  flex-direction: column;
-`;
+	width: 512px;
+	height: 768px;
+	position: relative;
+	background: white;
+	border-radius: 16px;
+	box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+	margin: 0 auto;
+	margin-top: 96px;
+	margin-bottom: 32px;
+	display: flex;
+	flex-direction: column;
+`
 
-export default ToDoTemplate;
+export default ToDoTemplate
